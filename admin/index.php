7d5cd8eb9b4da4ccc26799d7fe1de5b89c9f40ff
Mohammadmehdi-web <webauctech110@gmail.com>
2025-05-26 18:50:49 +0530

    add n files

--- admin/index.php
+++ admin/index.php
@@ -4,24 +4,24 @@
         include('../db_con.php');
         include('header.php');
         // Get total count of projects
-        // $total_blog = $con->query("SELECT COUNT(*) AS total FROM blogs")->fetch_assoc()['total'];
+        $total_banner = $con->query("SELECT COUNT(*) AS total FROM add_banner")->fetch_assoc()['total'];
         $total_query = $con->query("SELECT COUNT(*) AS total FROM contact")->fetch_assoc()['total'];
-        // $total_testimonials = $con->query("SELECT COUNT(*) AS total FROM add_testimonial")->fetch_assoc()['total'];
+        $total_gallery = $con->query("SELECT COUNT(*) AS total FROM gallery")->fetch_assoc()['total'];
         ?>
         <!-- END SIDEBAR-->
         <div class="content-wrapper">
             <!-- START PAGE CONTENT-->
             <div class="page-content fade-in-up">
                 <div class="row">
-                    <!-- <div class="col-lg-4 col-md-6">
+                    <div class="col-lg-4 col-md-6">
                         <div class="ibox bg-success color-white widget-stat">
                             <div class="ibox-body">
-                                <h2 class="m-b-5 font-strong"><?php echo $total_blog; ?></h2>
-                                <div class="m-b-5">Total Blogs</div><i class=" widget-stat-icon"><img width="50" height="50" src="https://img.icons8.com/ios-filled/50/FFFFFF/google-blog-search.png" alt="google-blog-search"/></i>
+                                <h2 class="m-b-5 font-strong"><?php echo $total_banner; ?></h2>
+                                <div class="m-b-5">Total Banners</div><i class=" widget-stat-icon"><img width="50" height="50" src="https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/FFFFFF/external-honorary-mentions-of-great-soldiers-in-a-flag-badges-bold-tal-revivo.png" alt="google-blog-search"/></i>
 
                             </div>
                         </div>
-                    </div> -->
+                    </div>
                     <div class="col-lg-4 col-md-6">
                         <div class="ibox bg-info color-white widget-stat">
                             <div class="ibox-body">
@@ -31,15 +31,15 @@
                             </div>
                         </div>
                     </div>
-                    <!-- <div class="col-lg-4 col-md-6">
+                    <div class="col-lg-4 col-md-6">
                         <div class="ibox bg-info color-white widget-stat">
                             <div class="ibox-body">
-                                <h2 class="m-b-5 font-strong"><?php echo $total_testimonials; ?></h2>
-                                <div class="m-b-5">Total Testimonials</div><i class="widget-stat-icon"><img width="50" height="50" src="https://img.icons8.com/external-kmg-design-glyph-kmg-design/32/FFFFFF/external-feedback-feedback-kmg-design-glyph-kmg-design.png"/></i>
+                                <h2 class="m-b-5 font-strong"><?php echo $total_gallery; ?></h2>
+                                <div class="m-b-5">Total Gallery</div><i class="widget-stat-icon"><img width="50" height="50" src="https://img.icons8.com/sf-black/64/FFFFFF/gallery.png"/></i>
 
                             </div>
                         </div>
-                    </div> -->
+                    </div>
                 </div>
             </div>
             <?php
